639c7ac9b48a0749c01e9f37b2dd17fefd80a66e
Sabrina Polazzo <sabrina_polazzo@outlook.com>
2024-02-18 01:05:14 -0300

    API com google adicionada

--- painel/login.php
+++ painel/login.php
@@ -1,6 +1,8 @@
 <?php
 
-if (isset($_COOKIE['remeber'])) {
+require('../vendor/autoload.php');
+
+if (isset($_COOKIE['remember'])) {
     $user = $_COOKIE['user'];
     $password = $_COOKIE['password'];
     $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.users` WHERE user = ? AND password = ? ");
@@ -33,6 +35,77 @@ if (isset($_COOKIE['remeber'])) {
 <body class="login">
     <div class="box-login">
         <h2>Login</h2>
+        <?php
+
+        $clientID = "343541106905-8k37g51dfbh8tpqbsh63ci78bvgmkuhk.apps.googleusercontent.com";
+        $clientSecret = "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";
+        $redirectUri = 'http://localhost/Sazzo/painel/main.php';
+
+        $client = new Google_Client();
+        $client->setClientId($clientID);
+        $client->setClientSecret($clientSecret);
+        $client->setRedirectUri($redirectUri);
+        $client->addScope('email profile');
+
+        $oauth2 = new Google_Service_Oauth2($client);
+
+        if (!isset($_GET['code'])) {
+            $authUrl = $client->createAuthUrl();
+            echo '<div class="login-google">
+            <a class="link-google" href="' . $authUrl . '"><i class="fa-brands fa-google"> Logar com Google</i></a>
+          </div>';
+        } else {
+            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+
+            if (!isset($token['error'])) {
+                $client->setAccessToken($token['access_token']);
+                $_SESSION['access_token'] = $token['access_token'];
+
+                // Define o tipo de usuário
+                $type_user = 0;
+
+                // Pega os dados todos os dados do usuário
+                $userInfo = $oauth2->userinfo->get();
+
+                // Pega os dados específicos para fazer o insert ou select no banco
+                $userId = $userInfo->getId();
+                $userEmail = $userInfo->getEmail();
+                $userName = $userInfo->getName();
+                $file = $userInfo->getPicture();
+
+                Painel::uploadFileGoogle($file, $userId);
+
+                // Verifica se há um usuário
+                $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.users_google` WHERE id_google = ?");
+                $sql->execute(array($userId));
+
+                if ($sql->rowCount() == 1) {
+                    $info = $sql->fetch();
+                    $_SESSION['login'] = true;
+                    $_SESSION['email'] = $info['email'];
+                    $_SESSION['name'] = $info['name'];
+                    $_SESSION['type_user'] = $info['type_user'];
+                    $_SESSION['img'] = $info['img'];
+                } else {
+                    $profilePicture = $userId . '_profile_picture.jpg';
+                    $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.users_google` VALUES (null,?,?,?,?,?) ");
+                    $sql->execute(array($userId, $userEmail, $userName, $profilePicture, $type_user));
+
+
+                    // Agora, faça uma nova consulta para obter as informações recém-inseridas
+                    $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.users_google` WHERE id_google = ?");
+                    $sql->execute(array($userId));
+                    $info = $sql->fetch();
+                    $_SESSION['login'] = true;
+                    $_SESSION['email'] = $info['email'];
+                    $_SESSION['name'] = $info['name'];
+                    $_SESSION['type_user'] = $info['type_user'];
+                    $_SESSION['img'] = $info['img'];
+                }
+            }
+        }
+        ?>
+
         <form method="POST">
             <input type="text" name="login" placeholder="Login...">
             <input type="password" name="password" placeholder="Senha...">
